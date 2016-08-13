1f08b52c82de391e0d0c0e1f4d95494c6d3fb017
tsouhaieb <tarhounisouhaieb@gmail.com>
2016-08-13 18:41:51 +0100

    first modification

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var alienImage: UIImageView!
+    @IBAction func updateImage(sender: AnyObject) {
+    
+        alienImage.image = UIImage(named: "Frame2")
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
